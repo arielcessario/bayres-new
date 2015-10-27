66df0c7113266359bb88fbafe1d7685f87db61a3
Mateo Maneff <mmaneff@gmail.com>
2015-10-26 23:27:17 -0200

    Agrego el modulo de usuarios

--- app/carrito/carrito.js
+++ app/carrito/carrito.js
@@ -17,15 +17,26 @@ angular.module('bayres.carrito', [
             data: {requiresLogin: true}
         });
     }])
-    .controller('CarritoController', CarritoController);
+    .controller('CarritoController', CarritoController)
+    .service('CarritoService', CarritoService);
 
-CarritoController.$inject = ['$routeParams', 'AcUtils', 'UserService', 'ProductService'];
+CarritoController.$inject = ['$routeParams', 'AcUtils', 'UserService', 'ProductService', 'CarritoService'];
+CarritoService.$inject = ['$http'];
 
-function CarritoController($routeParams, AcUtils, UserService, ProductService) {
+function CarritoController($routeParams, AcUtils, UserService, ProductService, CarritoService) {
     var vm = this;
 
     vm.productos = [];
+    vm.carritoDetalles = [];
+    vm.carritoInfo = {
+        cantidadDeProductos: 0,
+        totalAPagar: 0.00,
+        modified: false
+    };
+
     //vm.carrito = $routeParams.carrito;
+
+    vm.removeProducto = removeProducto;
 
     console.log(carrito);
 
@@ -33,4 +44,55 @@ function CarritoController($routeParams, AcUtils, UserService, ProductService) {
         console.log(data);
         vm.productos = data;
     });
+
+    function removeProducto(producto) {
+        if(vm.carritoDetalles.length > 0) {
+            var producto = vm.carritoDetalles[index];
+            var detalle = producto.nombre + ' $' + producto.precio_unitario + '(x' + producto.cantidad + ')';
+            var borrarOk = confirm('¿Desea borrar el producto '+ detalle +'?');
+            if(borrarOk){
+                vm.carritoDetalles.splice( index, 1 );
+                vm.carritoDetalles.sort(function(a, b){ return a.nombre - b.nombre; });
+                CarritoService.calcularCarritoTotal(vm.carritoDetalles, function(data){
+                    vm.carritoInfo = data;
+                });
+            } else {
+                return;
+            }
+        }
+    }
+
+
+}
+
+function CarritoService($http) {
+
+    //Variables
+    var service = {};
+
+    service.calcularCarritoTotal = calcularCarritoTotal;
+
+    return service;
+
+    function calcularCarritoTotal(carritoDetalles, callback) {
+        var carritoInfo = {
+            cantidadDeProductos: 0,
+            totalAPagar: 0.00,
+            modified: false
+        };
+        var cantidadDeProductos = 0;
+        var totalAPagar = 0.00;
+
+        carritoDetalles.forEach(function(data){
+            cantidadDeProductos = data.cantidad + cantidadDeProductos;
+            totalAPagar = (data.precio_unitario * data.cantidad)+ totalAPagar;
+        });
+
+        carritoInfo.cantidadDeProductos = cantidadDeProductos;
+        carritoInfo.totalAPagar = totalAPagar;
+        carritoInfo.modified = true;
+
+        callback(carritoInfo);
+    }
+
 }
